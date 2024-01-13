0ff14cfd772679a028648d837fc056c8e0e0548c
Dundo <107737568+ganud@users.noreply.github.com>
2024-01-13 10:53:20 -0500

    Game loop progress

--- src/dom.js
+++ src/dom.js
@@ -3,8 +3,8 @@ import { Player } from './player';
 import { Gameboard } from './gameboard';
 import { Ship } from './ship';
 
-export function renderBoard(boardDom, boardObject, isPlayer=true) {
-  boardDom.innerHTML = '';
+export function renderBoard(player, isPlayer=false) {
+  player.enemyDOM.innerHTML = '';
   for (let i = 0; i < 10; i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -12,10 +12,10 @@ export function renderBoard(boardDom, boardObject, isPlayer=true) {
       const square = document.createElement('div');
       square.classList.add('square');
       // If tile already attacked render it red
-      if (boardObject.fired.includes(`[${i},${j}]`)) {
+      if (player.enemyGameboard.fired.includes(`[${i},${j}]`)) {
         square.classList.add('hit');
       }
-      if (boardObject.isShip(i, j)) {
+      if (player.enemyGameboard.isShip(i, j)) {
         square.classList.add('ship');
       }
       square.setAttribute('value', `[${i},${j}]`);
@@ -23,12 +23,27 @@ export function renderBoard(boardDom, boardObject, isPlayer=true) {
       if (!isPlayer) {
         // Adjust tile behaviour here
         square.addEventListener('click', () => {
-          boardObject.receiveAttack(i, j);
-          renderBoard(boardDom, boardObject, isPlayer);
+          advanceTurn(humanplayer, enemyplayer, i, j)
         });
       }
       line.appendChild(square);
     }
-    boardDom.appendChild(line);
+    player.enemyDOM.appendChild(line);
   }
 };
+
+export function advanceTurn(player, enemy, x, y) {
+  player.attack(x,y);
+  enemy.randomMove();
+  renderBoard(player, false);
+  renderBoard(enemy, true);
+}
+
+const enemyBoard = document.getElementsByClassName('enemy-gameboard')[0];
+const enemyGameboard = new Gameboard();
+
+const playerBoard = document.getElementsByClassName('player-gameboard')[0];
+const playerGameboard = new Gameboard();
+
+const humanplayer = new Player(enemyGameboard, enemyBoard);
+const enemyplayer = new Player(playerGameboard, playerBoard);
--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import { Ship } from './ship';
 
 const enemyBoard = document.getElementsByClassName('enemy-gameboard')[0];
 const enemyGameboard = new Gameboard();
-renderBoard(enemyBoard, enemyGameboard, false);
 
 const playerBoard = document.getElementsByClassName('player-gameboard')[0];
 const playerGameboard = new Gameboard();
-renderBoard(playerBoard, playerGameboard, true);
 
 const player = new Player(enemyGameboard, enemyBoard);
 const enemy = new Player(playerGameboard, playerBoard);
+renderBoard(player, false);
+renderBoard(enemy, true);
+
 
 
 // Game loop
